eb8ac50f70456ee869c6207bbaedb799779a51a6
chethan_jagannath <chethanjagannath1988@gmail.com>
2018-08-20 20:00:12 +0530

    ProjectCreation

---
+++ src/main/java/com/demo/project/ImpactAnalysisApplication.java
@@ -0,0 +1,12 @@
+package com.demo.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpactAnalysisApplication.class, args);
+	}
+}
