92148f24d99d69e46a4983c65f81bd194e7b07b0
chethan_jagannath <chethanjagannath1988@gmail.com>
2018-08-21 22:54:13 +0530

    Added an API - PostCommitDetails

--- src/main/java/com/impactanalysis/controllers/GitController.java
+++ src/main/java/com/impactanalysis/controllers/GitController.java
@@ -1,7 +1,11 @@
 package com.impactanalysis.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/ImpactAnalysis")
 public class GitController {
 	
-	//@GetMapping(value="/Test", produces =  "application/json", consumes =  "application/json")
+	private static final Logger logger = LoggerFactory.getLogger(GitController.class);
+	
 	@GetMapping("/Test")
 	public String testApp() {
 		return "Success - ImpactAnalysis Application is Up & Running";
+	}
+	
+	@PostMapping(value="/PostCommitDetails", consumes =  "application/json")
+	public void postCommitDetails(@RequestBody Object obj) {
+		logger.info("Printing PostCommitDetails" + obj);
 	}
 
 }
