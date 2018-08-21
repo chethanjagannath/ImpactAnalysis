f2157a472021dd7c1e1bf9756964048f7fd648f1
chethan_jagannath <chethanjagannath1988@gmail.com>
2018-08-21 13:19:14 +0530

    Added Test Method

--- src/main/java/com/impactanalysis/controllers/GitController.java
+++ src/main/java/com/impactanalysis/controllers/GitController.java
@@ -1,5 +1,18 @@
 package com.impactanalysis.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value="/ImpactAnalysis")
 public class GitController {
+	
+	@GetMapping(value="/Test", produces =  "application/json", consumes =  "application/json")
+	public String testApp() {
+		return "Success - ImpactanAnalysis Application is Up & Running";
+	}
 
 }
