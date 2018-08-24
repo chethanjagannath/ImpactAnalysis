2320cf886b5578474860848436f2e79d37d507b6
Anil Chowdhary <anil.chowdhary@lntinfotech.com>
2018-08-24 12:11:59 +0530

    IA: Code clean up

--- src/main/java/com/impactanalysis/clients/GitClient.java
+++ src/main/java/com/impactanalysis/clients/GitClient.java
@@ -2,8 +2,7 @@ package com.impactanalysis.clients;
 
 import java.io.Serializable;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -41,78 +41,47 @@ public class GitClient implements Serializable {
 
 	public GitResponseDTO getCommitDetailsByDate(GitRequestDTO gitRequestDTO) {
 
-		/*	gitURI = "https://api.github.com/search/commits?q=repo:chethanjagannath/ImpactAnalysis+committer-date:2018-08-21";
-		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
-		headers.add("Content-Type", "application/vnd.github.cloak-preview");
-		headers.add("Accept", MediaTypeSupport.GITHUB_MEDIATYPE);
-		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-
-		HttpEntity<GitUserDTO> request = new HttpEntity<GitUserDTO>(gitUserDTO, headers);
-
-		
-		logger.info("Printing getCommitDetailsByDate" + gitUserDTO);
-		try {
-			restTemplate.getForObject(gitURI + "search/commits", IAResponse.class, request);
-			//restTemplate.getForObject(gitURI + "search/commits", IAResponse.class);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}*/
-
-		Map<String, Object> params = new HashMap<>();
-
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.setAccept(Collections.singletonList(MediaTypeSupport.GITHUB_MEDIATYPE));
 
-		params.put("repo:", gitRequestDTO.getOwnerId() + "/" + gitRequestDTO.getProjectRepo());
-		params.put("+committer-date:", gitRequestDTO.getCommitDate());
-
 		HttpEntity<GitRequestDTO> request = new HttpEntity<GitRequestDTO>(null, httpHeaders);
-		params.put("httpHeaders", request);
 
 		restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter);
-		/*
-		 * restTemplate.setRequestFactory( new
-		 * HttpComponentsClientHttpRequestFactory());
-		 */
 
 		GitResponseDTO responseEntity = null;
 		String URI = gitURI + "/search/commits?q=repo:" + gitRequestDTO.getOwnerId() + "/"
 				+ gitRequestDTO.getProjectRepo() + "+committer-date:" + gitRequestDTO.getCommitDate();
+
 		logger.info("RequestDetails:" + gitRequestDTO + " URI :  " + URI);
+
 		try {
 			responseEntity = restTemplate.getForObject(URI, GitResponseDTO.class, request);
 		} catch (HttpClientErrorException e) {
-			// success = false;
-			// Log.e(TAG, command+" was rejected for URL: "+url, e);
 			System.out.println(e.getStatusCode().value());
 			System.out.println(e.getResponseBodyAsString());
 		} catch (HttpServerErrorException e) {
 			System.out.println(e.getStatusCode().value());
 			System.out.println(e.getResponseBodyAsString());
 		}
-		// HttpEntity<String> entity = new HttpEntity<>("parameters", httpHeaders);
-		// Object obj = restTemplate.exchange(gitURI + "/search/commits",
-		// HttpMethod.GET, entity, Object.class);
+
 		logger.info("Response Object:" + responseEntity);
 
 		return responseEntity;
 	}
-	
+
 	public GitResponseDTO getCommitDetailsByCommitId(String commitId) {
 
-		Map<String, Object> params = new HashMap<>();
-
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.setAccept(Collections.singletonList(MediaTypeSupport.GITHUB_MEDIATYPE_MERCY));
 
 		HttpEntity<GitRequestDTO> request = new HttpEntity<GitRequestDTO>(null, httpHeaders);
-		params.put("httpHeaders", request);
 
 		restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter);
 
 		GitResponseDTO responseEntity = null;
-		String URI = gitURI + "/repos/chethanjagannath/ImpactAnalysis/commits/"+commitId;
-	 	logger.info(" URI :  " + URI);
+		String URI = gitURI + "/repos/chethanjagannath/ImpactAnalysis/commits/" + commitId;
+
+		logger.info(" URI :  " + URI);
 		try {
 			responseEntity = restTemplate.getForObject(URI, GitResponseDTO.class, request);
 		} catch (HttpClientErrorException e) {
@@ -126,22 +95,20 @@ public class GitClient implements Serializable {
 
 		return responseEntity;
 	}
-	
-	public GitResponseDTO getCommitDetailsBetweenCommitIds(String commitId1, String commitId2) {
 
-		Map<String, Object> params = new HashMap<>();
+	public GitResponseDTO getCommitDetailsBetweenCommitIds(String commitId1, String commitId2) {
 
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.setAccept(Collections.singletonList(MediaTypeSupport.GITHUB_MEDIATYPE_MERCY));
 
 		HttpEntity<GitRequestDTO> request = new HttpEntity<GitRequestDTO>(null, httpHeaders);
-		params.put("httpHeaders", request);
 
 		restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter);
 
 		GitResponseDTO responseEntity = null;
-		String URI = gitURI + "/repos/chethanjagannath/ImpactAnalysis/compare/"+commitId1+"..."+commitId2;
-	 	logger.info(" URI :  " + URI);
+		String URI = gitURI + "/repos/chethanjagannath/ImpactAnalysis/compare/" + commitId1 + "..." + commitId2;
+
+		logger.info(" URI :  " + URI);
 		try {
 			responseEntity = restTemplate.getForObject(URI, GitResponseDTO.class, request);
 		} catch (HttpClientErrorException e) {
@@ -151,6 +118,7 @@ public class GitClient implements Serializable {
 			System.out.println(e.getStatusCode().value());
 			System.out.println(e.getResponseBodyAsString());
 		}
+
 		logger.info("Response Object:" + responseEntity);
 
 		return responseEntity;
