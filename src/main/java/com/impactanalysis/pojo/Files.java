b919651b095f864cc9db79f3ede44410e59ab10f
chethan_jagannath <chethanjagannath1988@gmail.com>
2018-08-31 15:28:14 +0530

    .

--- src/main/java/com/impactanalysis/pojo/Files.java
+++ src/main/java/com/impactanalysis/pojo/Files.java
@@ -7,10 +7,10 @@ public class Files implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private String filename;
-//	private String status;
-//	private int additions;
-//	private int deletions;
-//	private int changes;
+	private String status;
+	private int additions;
+	private int deletions;
+	private int changes;
 
 	public String getFilename() {
 		return filename;
@@ -20,35 +20,35 @@ public class Files implements Serializable{
 		this.filename = filename;
 	}
 
-//	public String getStatus() {
-//		return status;
-//	}
-//
-//	public void setStatus(String status) {
-//		this.status = status;
-//	}
-//
-//	public int getAdditions() {
-//		return additions;
-//	}
-//
-//	public void setAdditions(int additions) {
-//		this.additions = additions;
-//	}
-//
-//	public int getDeletions() {
-//		return deletions;
-//	}
-//
-//	public void setDeletions(int deletions) {
-//		this.deletions = deletions;
-//	}
-//
-//	public int getChanges() {
-//		return changes;
-//	}
-//
-//	public void setChanges(int changes) {
-//		this.changes = changes;
-//	}
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getAdditions() {
+		return additions;
+	}
+
+	public void setAdditions(int additions) {
+		this.additions = additions;
+	}
+
+	public int getDeletions() {
+		return deletions;
+	}
+
+	public void setDeletions(int deletions) {
+		this.deletions = deletions;
+	}
+
+	public int getChanges() {
+		return changes;
+	}
+
+	public void setChanges(int changes) {
+		this.changes = changes;
+	}
 }
